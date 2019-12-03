a6bd9db8b4d0c67774ad90914bf79b2b3127063f
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-03 17:31:53 -0500

    add app/routes helper

---
+++ src/data/app/routes.js
@@ -0,0 +1,43 @@
+/// internal modules ///
+import check from '../../tools/iffy/is-type';
+
+export const routes = {
+  here : {},
+  there : {
+    CORS : 'https://cors-anywhere.herokuapp.com/',
+    GH : {
+      base : 'https://api.github.com',
+      path : {
+        user : (username) => (`/users/${username}`),
+      }
+    },
+  },
+};
+
+export const fullURL = (domain , point , ...args) => {
+  // simply return domain as url if it's a string
+  if (check.isString (domain)) {
+    return (domain);
+  }
+  // compose url if domain looks right
+  else if (domain && check.isString (domain.base)) {
+    let base = domain.base;
+    let path = '';
+
+    // add point if it's a String
+    if (check.isString (point)) {
+      path = domain.path[point];
+    }
+
+    // use args in endpoint
+    if (check.isFunction (path)) {
+      path = path (...args);
+    }
+
+    return (base + path);
+  }
+  // otherwise
+  else {
+    return (null);
+  }
+};
