41a902051fcea0c77acba0dda50dba6785b7bc42
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-03 17:17:41 -0500

    convert App to class component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,35 @@ import './styles/App.css';
 /***************************************
   COMPONENT
 ***************************************/
-function App () {
-  return (
-    <div className='App'>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor (props) {
+    console.log (`>>> App : constructing... <<<`);
+    super (props);
+    this.state = {
+    };
+  };
+
+  componentDidMount () {
+    console.log (`>>> App : did mount... <<<`);
+  };
+
+  componentDidUpdate () {
+    console.log (`>>> App : did update... <<<`);
+  };
+
+  componentWillUnmount () {
+    console.log (`>>> App : will unmount... <<<`);
+  };
+
+  render () {
+    console.log (`>>> App : rendering... <<<`);
+    return (
+      <div className='App'>
+        App
+      </div>
+    );
+  };
+};
 
 /**************************************/
 export default App;
