6e41a0f518b286186e0205532a75c476c779b9a8
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-04 09:28:21 -0500

    add setStateData fun

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import axios from 'axios';
 
 /// internal modules ///
-// import remote from './tools/remotely/remote';
+import immutably from './tools/immutably';
 
 /// styles ///
 import './styles/App.css';
